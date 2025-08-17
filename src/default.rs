dd828b5feb1b2261a1f526fcc026d9b97bff2934
jonasrsv42 <jonasrsv42@gmail.com>
2025-08-17 16:13:46 +0200

    asdfasdfasdfasdfasdfasdfadsf</p>

--- src/default.rs
+++ src/default.rs
@@ -1,51 +1,55 @@
 use super::parser::Parser;
-use crate::ByteCursor;
-use crate::ParsicombError;
+use crate::{Atomic, Cursor, ParsicombError};
 
 /// Parser that always succeeds without consuming input and returns the default value of T
-pub struct DefaultParser<T> {
-    _phantom: std::marker::PhantomData<T>,
+pub struct DefaultParser<T, C> {
+    default: T,
+    _phantom_cursor: std::marker::PhantomData<C>,
 }
 
-impl<T> DefaultParser<T> {
-    pub fn new() -> Self {
+impl<'code, T, C> DefaultParser<T, C> {
+    pub fn new(default: T) -> Self {
         DefaultParser {
-            _phantom: std::marker::PhantomData,
+            default,
+            _phantom_cursor: std::marker::PhantomData,
         }
     }
 }
 
-impl<'code, T> Parser<'code> for DefaultParser<T>
+impl<'code, T, C> Parser<'code> for DefaultParser<T, C>
 where
-    T: Default,
+    T: Clone,
+    C: Cursor<'code>,
+    C::Element: Atomic + 'code,
 {
-    type Cursor = ByteCursor<'code>;
+    type Cursor = C;
     type Output = T;
-    type Error = ParsicombError<'code>;
+    type Error = ParsicombError<'code, <C as Cursor<'code>>::Element>;
 
     fn parse(&self, cursor: Self::Cursor) -> Result<(Self::Output, Self::Cursor), Self::Error> {
-        Ok((T::default(), cursor))
+        Ok((self.default.clone(), cursor))
     }
 }
 
 /// Convenience function to create a default parser
-pub fn default<T>() -> DefaultParser<T>
+pub fn default<'code, T, C>(default: T) -> DefaultParser<T, C>
 where
-    T: Default,
+    T: Clone,
+    C: Cursor<'code>,
 {
-    DefaultParser::new()
+    DefaultParser::new(default)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::Cursor;
+    use crate::{ByteCursor, Cursor};
 
     #[test]
     fn test_default_string() {
         let data = b"hello";
         let cursor = ByteCursor::new(data);
-        let parser = default::<String>();
+        let parser = default(String::default());
 
         let (result, remaining) = parser.parse(cursor).unwrap();
         assert_eq!(result, String::default());
@@ -57,7 +61,7 @@ mod tests {
     fn test_default_i32() {
         let data = b"123";
         let cursor = ByteCursor::new(data);
-        let parser = default::<i32>();
+        let parser = default(i32::default());
 
         let (result, remaining) = parser.parse(cursor).unwrap();
         assert_eq!(result, 0);
@@ -69,7 +73,7 @@ mod tests {
     fn test_default_empty_input() {
         let data = b"";
         let cursor = ByteCursor::new(data);
-        let parser = default::<String>();
+        let parser = default(String::default());
 
         let (result, remaining) = parser.parse(cursor).unwrap();
         assert_eq!(result, String::default());
